5f1cc2e2c8536ad51ce9ce5460a177db14496711
Yanuar <yanuar@damcorp.id>
2020-03-05 11:26:49 +0700

    add example

--- features/default.js
+++ features/default.js
@@ -4,5 +4,7 @@ const httpContext = require('express-http-context')
 const uuid = require('uuid/v4')
 const tm = require('../helper/trackingManager')
 module.exports = (controller) => {
-	
+	controller.plugins.cms.before("default", "final", async(convo, bot) => {
+
+	})
 }
